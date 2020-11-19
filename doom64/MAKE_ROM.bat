2f03e0a304a1d0115c50762d6684b8f66d906c8f
Erick Vásquez García <41172072+Erick194@users.noreply.github.com>
2020-11-19 16:57:18 -0600

    Add missing MAKE_ROM.bat file

---
+++ doom64/MAKE_ROM.bat
@@ -0,0 +1,37 @@
+@echo off
+
+::Set Main Root:
+
+set ROOT=c:\ultra
+
+::Setup GCC:
+set gccdir=%ROOT%\gcc
+path %gccdir%\mipse\bin;%path%
+set gccsw=-mips3 -mgp32 -mfp32 -funsigned-char -D_LANGUAGE_C -D_ULTRA64 -D__EXTENSIONS__
+set n64align=on
+set GCC_CELF=ON
+
+::Setup LIB:
+path %root%\usr\sbin;%path%
+
+rem set Microsoft Visual C Ver 4.2 install directory
+rem set MSVC=d:\msvc
+
+rem set Borland C Ver 5.0 install directory
+rem set BCC=d:\bc5
+
+::Set mypath:
+cd %mypath%
+
+@echo on
+
+make
+
+@echo off
+::remove_all:
+	del *.o
+	del *.elf
+	del *.tbl
+@echo on
+
+pause
