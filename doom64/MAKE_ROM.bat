a528c1fb1df25eb964c17d4c9c2560b15e9e4419
CrashOveride95 <crashoveride953@gmail.com>
2020-12-11 21:17:00 -0500

    Add NRDC

--- doom64/MAKE_ROM.bat
+++ doom64/MAKE_ROM.bat
@@ -14,12 +14,6 @@ set GCC_CELF=ON
 ::Setup LIB:
 path %root%\usr\sbin;%path%
 
-rem set Microsoft Visual C Ver 4.2 install directory
-rem set MSVC=d:\msvc
-
-rem set Borland C Ver 5.0 install directory
-rem set BCC=d:\bc5
-
 ::Set mypath:
 cd %mypath%
 
