6931e678a0b2958be1b49598f2fe60712c6596e1
Erick Vásquez García <41172072+Erick194@users.noreply.github.com>
2025-05-29 23:04:00 -0600

    Update p_inter.c

--- doom64/p_inter.c
+++ doom64/p_inter.c
@@ -381,7 +381,7 @@ void P_TouchSpecialThing (mobj_t *special, mobj_t *toucher) // 80014810
 	case MT_AMMO_SHELLBOX:
 		if (!P_GiveAmmo (player, am_shell,5))
 			return;
-		message = "Picked up a box of shotgun shells.";
+		message = "Picked up a box of shells.";
 		break;
 	case MT_AMMO_BACKPACK:
 		if (!player->backpack)
